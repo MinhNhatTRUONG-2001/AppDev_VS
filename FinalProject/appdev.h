9d30b36af39d472a9615bb203ea55f160ff4a8ed
MinhNhatTRUONG-2001 <82155278+MinhNhatTRUONG-2001@users.noreply.github.com>
2021-04-09 10:11:41 +0300

    Update appdev.h

--- FinalProject/appdev.h
+++ FinalProject/appdev.h
@@ -8,10 +8,12 @@
 #define _CRT_SECURE_NO_WARNINGS
 #define N 20
 //added from VS
+// modified from github
 typedef struct {
 	int max;
 	int min;
 	char rname[N];
 }rock;
 
+//function prototypes
 void send_data(rock);
